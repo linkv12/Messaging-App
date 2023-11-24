ca55c7db9221155a8fe95d45f43b75b02b7cadc3
linkv12 <thehellsingingslicer@gmail.com>
2023-11-24 16:01:16 +0800

    chore: add comment to elgamal

--- src/encrypt/elgamal.py
+++ src/encrypt/elgamal.py
@@ -1,9 +1,11 @@
+# Implementation by Shamgar Otherson D.
+
 import random
 import json
 
 
 class ElGamal:
-    # Static class variable
+    # * Static class variable
     encrypted_message_bytes = bytes("2eg", "UTF-16")
     pub_key_bytes = bytes("5eg", "UTF-16")
 
@@ -20,19 +22,31 @@ class ElGamal:
         :param self:            Attributes Instance
         """
 
+        # * Public modulo
+        # * Public base
         self.public_modulo = self.generate_public_modulo()
         self.public_base = self.generate_public_base()
 
-        # flag len -> 8
+        # * Flag len -> 8
         self.pub_key_bytes = bytes("5eg", "UTF-16")
         self.encrypted_message_bytes = bytes("2eg", "UTF-16")
 
+        # * Key, private, public etc.
         self.private_key = self.generate_private_key()
         self.base_public_key = self.generate_base_public_key()
         self.public_key = self.generate_publlic_key()
 
     # * Variable generation
     def generate_public_modulo(self):
+        """
+        Generate public modulo
+        A very large prime number
+
+        :param self:            Attributes Instance
+
+        :return:                public modulo
+        """
+
         p_mod = random.randint(pow(10, 20), pow(10, 50))
         p_mod_prime = self.is_prime(p_mod, 13)
 
@@ -43,6 +57,15 @@ class ElGamal:
         return p_mod
 
     def generate_public_base(self):
+        """
+        Generate public base
+        A primitive root of public modulo
+
+        :param self:            Attributes Instance
+
+        :return:                public base
+        """
+
         p_base = random.randint(pow(10, 20), self.public_modulo)
         while self.gcd(p_base, self.public_modulo) != 1:
             p_base = random.randint(pow(10, 20), self.public_modulo)
@@ -51,13 +74,36 @@ class ElGamal:
 
     # * KEY
     def generate_private_key(self):
+        """
+        Generate private key
+        a random number between 2 to publoc modulo -1
+
+        :param self:            Attributes Instance
+
+        :return:                private key
+        """
         return random.randint(2, self.public_modulo - 1)
 
     def generate_base_public_key(self):
-        # modular exponentiation of public_base^private_key mod public_modulo
+        """
+        Generate base public key
+        A modular exponentiation of public base, private key and public modulo
+
+        :param self:            Attributes Instance
+
+        :return:                base public key
+        """
         return pow(self.public_base, self.private_key, self.public_modulo)
 
     def generate_publlic_key(self):
+        """
+        Generate public key
+        A dictionary that contain public modulo, public base and base public key
+
+        :param self:            Attributes Instance
+
+        :return:                public key, a dict
+        """
         # public key is tuple of public_modulo[the bigest number]
         #                        public_base is primitive root of public_modulo
         #                        base_public_key is pow(public_base, private_key, public modulo)
@@ -72,6 +118,16 @@ class ElGamal:
     # * Encryption & Decryption
     @classmethod
     def encrypt(cls, message: str, public_key: dict):
+        """
+        Encrypt text through a class method
+        with message and public key as parameter
+
+        :param cls:         Class Instances
+        :param message:     String to be enrypted
+        :param public_key:  Public key used for encryption
+
+        :return:            list that contain cipher_1 and cipher_2 in that order
+        """
         # pow (public_base, private_key, public_modulo)
         base_pub_key = public_key["base_public_key"]
         emphereal_key = random.randint(1, public_key["public_modulo"] - 1)
@@ -89,6 +145,14 @@ class ElGamal:
         return [cipher_1, cipher_2]
 
     def decrypt(self, ciphers: list):
+        """
+        Decrypt cipher to raw string
+
+        :param self:        Attributes Instance
+        :param ciphers:     ciphers is a list that contain cipher_1 and cipher_2
+
+        :return:            message, decrypted
+        """
         # calculate shared key
         cipher_1, cipher_2 = ciphers
         shared_secret = self.power(cipher_1, self.private_key, self.public_modulo)
@@ -101,6 +165,15 @@ class ElGamal:
     # * Pack & unpack
     # * pack -> bytes
     def pack_public_key(self, encoding="utf-8"):
+        """
+        Pack public key to bytes using json library
+
+        :param self:        Attributes Instance
+        :param encoding:    encoding method for str
+
+        :return:            packed public key a bytes object
+        """
+
         encoded_pub_key = (
             json.dumps(self.public_key).encode(encoding) + self.pub_key_bytes
         )
@@ -108,6 +181,15 @@ class ElGamal:
 
     @classmethod
     def unpack_public_key(cls, encoded_pub_key, encoding="utf-8"):
+        """
+        Unpack bytes object that is presumed to be public key
+
+        :param cls:                 Class Instances
+        :param encoded_pub_key:     packed public key, bytes object
+        :param encoding:            encoding method that used for packing public key
+
+        :return:                    dict object a proper public key
+        """
         # still contain pub_key_bytes,
         # already verified ending with b""5eg"
         encoded_pub_key = encoded_pub_key[:-8].decode(encoding)
@@ -116,6 +198,16 @@ class ElGamal:
 
     @classmethod
     def pack_encrypted_message(cls, ciphers: list, encoding="utf-8"):
+        """
+        Pack encrypted message, in form of ciphers
+
+        :param cls:                 Class Instances
+        :param ciphers:             list object contain cipher_1 and cipher_2
+        :param encoding:            encoding method
+
+        :return:                    packed ciphers a bytes object
+        """
+
         # ciphers -> [cipher1, cipher2]
 
         # we pack it to send
@@ -126,14 +218,34 @@ class ElGamal:
 
     @classmethod
     def unpack_encrypted_message(cls, packed_ciphers, encoding="utf-8"):
+        """
+        Unpack encrypted message, in form of bytes
+
+        :param cls:                 Class Instances
+        :param packed_ciphers:      bytes object contain cipher_1 and cipher_2
+        :param encoding:            encoding method
+
+        :return:                    ciphers
+        """
+        # does it need to be class method instead of using instance method?
         packed_ciphers = packed_ciphers[:-8].decode(encoding)
         ciphers = json.loads(packed_ciphers)
         return ciphers
 
     @classmethod
     def pack_to_bytes_message(cls, message, public_key, encoding="utf-8"):
+        """
+        Unpack encrypted message, in form of bytes
+
+        :param cls:                 Class Instances
+        :param message:             String to be enrypted
+        :param public_key:          Public key used for encryption
+        :param encoding:            encoding method
+
+        :return:                    pack encrypted bytes
+        """
         ciphers = ElGamal.encrypt(message, public_key)
-        return ElGamal.pack_encrypted_message(ciphers)
+        return ElGamal.pack_encrypted_message(ciphers, encoding=encoding)
 
     # * UTILS
     def gcd(self, a, b):
@@ -194,20 +306,3 @@ class ElGamal:
                 return False
 
         return True
-
-
-if __name__ == "__main__":
-    eg_1 = ElGamal()
-
-    pac_pub = eg_1.pack_public_key()
-    pub_key = eg_1.unpack_public_key(pac_pub)
-
-    print(pub_key == eg_1.public_key)
-    # enc mess
-
-    mess = "heyoooo!"
-    enc_ = eg_1.encrypt(mess, pub_key)
-    pac_mess = ElGamal.pack_encrypted_message(ciphers=enc_)
-    enc_u = eg_1.unpack_encrypted_message(pac_mess)
-
-    print(enc_u == enc_)
